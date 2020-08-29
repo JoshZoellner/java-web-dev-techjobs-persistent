4e68b71622b8eceee1fcbc6454cfddc1c97e6c3e
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-08-29 15:37:34 -0400

    added controllers and updated models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull(message = "Description is required")
+    private String description;
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
